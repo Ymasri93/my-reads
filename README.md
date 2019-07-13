daf3c2c60bbfbf559cc17443b64198e2b1a4c5fe
Ymasri <yasir.masri@gmail.com>
2019-07-13 20:23:16 +0300

    adding prettifier with bookshelf

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# My Reads
+A book tracking App.
+
 
 ## Available Scripts
 
---
+++ src/BooksShelf.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BooksShelf.css';
+
+function BooksShelf() {
+  return (
+    <div className="miniwrapper">
+      <div className="bookshelf" />
+
+      <div className="bookshelf" />
+    </div>
+  );
+}
+
+export default BooksShelf;
